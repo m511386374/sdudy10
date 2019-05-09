aab83aaaf505d8928381266c14a7097ae2d17f47
NickMing <511386374@qq.com>
2019-05-09 09:55:19 +0800

    Initial commit

---
+++ src/main/java/com/example/sdudy10/Sdudy10Application.java
@@ -0,0 +1,13 @@
+package com.example.sdudy10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdudy10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdudy10Application.class, args);
+    }
+
+}
